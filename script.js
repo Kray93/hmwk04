9d83cab6128a7075d59f3e62aa969bb4ead351e5
Kray93 <kevinwillig@gmail.com>
2020-12-13 21:24:28 -0800

    added questions

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("is it linked??");
+var playBtn = document.querySelector("#playBtn")
+
+
+playGame()
+
+playBtn.addEventListener("click", function playGame(event) {
+    event.preventDefault();
+    
+});
